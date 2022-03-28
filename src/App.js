5ccce3cd39abad8df22f1943aeffaa25495ca8c4
developermkarim <m.karimcu@gmail.com>
2022-03-29 00:47:10 +0600

    React-Rounter-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Friends from './Components/Friends/Friends';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import NotFriends from './Components/Friends/NotFriends';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To my Fancy Routing Website</h1>
+    {/*  <Friends></Friends> */}
+    <Routes>
+      <Route path='/Home' element={<Home/>}></Route>
+      <Route path='/friends' element={<Friends/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='*' element={<NotFriends></NotFriends>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/Components/Friends/NotFriends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFriends = () => {
+    return (
+        <div>
+            <h2> The page you are tring not Found</h2>
+            <h2>404</h2>
+        </div>
+    );
+};
+
+export default NotFriends;
